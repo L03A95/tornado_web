41cf059f90de7e958841cf1258be8e8ce17bb7c2
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-10-11 22:24:10 -0300

    Fixed Menu and Header

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -14,11 +14,11 @@ export default function Menu () {
 
     return (
         <>
-            <h1 className='menu_title'>Menú</h1>
+            <h1 className='menu_title'>~Menú~</h1>
             <div>
                 <section className='section_wrapper'>
                     <span className='section_title'>Entrada</span>
-                    {menus.map((m : any) => {
+                    {menu.map((m : any) => {
                         if(m.category == 'entrada') {
                             return <MenuCard name={m.name} description={m.description} image={m.image} price={m.price} />
                         }
@@ -27,7 +27,7 @@ export default function Menu () {
 
                 <section className='section_wrapper'>
                     <span className='section_title'>Parrilla</span>
-                    {menus.map((m : any) => {
+                    {menu.map((m : any) => {
                         if(m.category == 'parrilla') {
                             return <MenuCard name={m.name} description={m.description} image={m.image} price={m.price} />
                         }
@@ -36,7 +36,7 @@ export default function Menu () {
 
                 <section className='section_wrapper'>
                     <span className='section_title'>Dulces</span>
-                    {menus.map((m : any) => {
+                    {menu.map((m : any) => {
                         if(m.category == 'dulces') {
                             return <MenuCard name={m.name} description={m.description} image={m.image} price={m.price} />
                         }
@@ -45,7 +45,7 @@ export default function Menu () {
 
                 <section className='section_wrapper'>
                     <span className='section_title'>Cafetería</span>
-                    {menus.map((m : any) => {
+                    {menu.map((m : any) => {
                         if(m.category == 'cafeteria') {
                             return <MenuCard name={m.name} description={m.description} image={m.image} price={m.price} />
                         }
@@ -54,7 +54,7 @@ export default function Menu () {
 
                 <section className='section_wrapper'>
                     <span className='section_title'>Vinos</span>
-                    {menus.map((m : any) => {
+                    {menu.map((m : any) => {
                         if(m.category == 'vinos') {
                             return <MenuCard name={m.name} description={m.description} image={m.image} price={m.price} />
                         }
