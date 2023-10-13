5955ec0f9df66010810e7523b4aeb341da46356a
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-10-13 18:51:11 -0300

    Changed Header and home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ export default function Home () {
 
     return (
         <>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum perferendis ex quasi autem nostrum dignissimos! Laudantium quidem mollitia voluptatibus exercitationem? Assumenda molestiae rem delectus non a maiores, veritatis eum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid esse dignissimos at illo, illum minima corporis blanditiis dolore sint ea non molestias consectetur eligendi earum magnam! Ab, quia magnam!</p>
         </>
     )
 }
