093f7c845bf763d91a38c8d9dc833257a4482582
Liam M. Perez Lupia <liamperezlupia53@gmail.com>
2023-11-06 23:24:39 -0300

    Started carrousel

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,6 +9,7 @@ export default function Home () {
             <p>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero accusantium voluptatem saepe. Numquam, illum voluptatibus, doloremque vero minima harum perspiciatis cupiditate tenetur placeat aliquid, dolores voluptatum voluptates deserunt alias nihil sit? Ab reiciendis aliquid necessitatibus impedit alias officiis facilis culpa cum, possimus distinctio perspiciatis saepe ex sunt consequuntur veniam tempora dolorem asperiores. Molestias tempore voluptatibus voluptatem sint consequuntur impedit iste officiis iusto doloremque provident, unde optio mollitia quod quaerat quas eaque rerum, ad ea consequatur, voluptas natus doloribus quos exercitationem. Ut atque ipsa voluptate mollitia. Obcaecati repellendus, reprehenderit molestiae illum quaerat quas neque exercitationem dolores consectetur incidunt voluptate, quo non.
             </p>
+            <span>Carrousel</span>
         </main>
     )
 }
